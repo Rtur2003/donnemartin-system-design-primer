d86b798138722a8971bd6cba72435fba850b7d0b
Rtur2003 <hasannarthurrr@gmail.com>
2025-12-07 14:40:12 +0300

    implement spot allocation and vehicle lifecycle

--- solutions/object_oriented_design/parking_lot/parking_lot.py
+++ solutions/object_oriented_design/parking_lot/parking_lot.py
@@ -14,12 +14,12 @@ class Vehicle(metaclass=ABCMeta):
     def __init__(self, vehicle_size, license_plate, spot_size):
         self.vehicle_size = vehicle_size
         self.license_plate = license_plate
-        self.spot_size
+        self.spot_size = spot_size
         self.spots_taken = []
 
     def clear_spots(self):
         for spot in self.spots_taken:
-            spot.remove_vehicle(self)
+            spot.remove_vehicle()
         self.spots_taken = []
 
     def take_spot(self, spot):
@@ -45,7 +45,7 @@ class Car(Vehicle):
         super(Car, self).__init__(VehicleSize.COMPACT, license_plate, spot_size=1)
 
     def can_fit_in_spot(self, spot):
-        return True if (spot.size == LARGE or spot.size == COMPACT) else False
+        return spot.vehicle_size in (VehicleSize.LARGE, VehicleSize.COMPACT)
 
 
 class Bus(Vehicle):
@@ -54,17 +54,17 @@ class Bus(Vehicle):
         super(Bus, self).__init__(VehicleSize.LARGE, license_plate, spot_size=5)
 
     def can_fit_in_spot(self, spot):
-        return True if spot.size == LARGE else False
+        return spot.vehicle_size == VehicleSize.LARGE
 
 
 class ParkingLot(object):
 
     def __init__(self, num_levels):
         self.num_levels = num_levels
-        self.levels = []  # List of Levels
+        self.levels = [Level(floor, Level.SPOTS_PER_ROW * 5) for floor in range(num_levels)]
 
     def park_vehicle(self, vehicle):
-        for level in levels:
+        for level in self.levels:
             if level.park_vehicle(vehicle):
                 return True
         return False
@@ -77,8 +77,21 @@ class Level(object):
     def __init__(self, floor, total_spots):
         self.floor = floor
         self.num_spots = total_spots
-        self.available_spots = 0
+        self.available_spots = total_spots
         self.spots = []  # List of ParkingSpots
+        self._init_spots(total_spots)
+
+    def _init_spots(self, total_spots):
+        """Initialize rows of spots with a simple distribution across sizes."""
+        for spot_number in range(total_spots):
+            row = spot_number // self.SPOTS_PER_ROW
+            if spot_number < total_spots * 0.25:
+                size = VehicleSize.MOTORCYCLE
+            elif spot_number < total_spots * 0.5:
+                size = VehicleSize.COMPACT
+            else:
+                size = VehicleSize.LARGE
+            self.spots.append(ParkingSpot(self, row, spot_number, 1, size))
 
     def spot_freed(self):
         self.available_spots += 1
@@ -88,16 +101,39 @@ class Level(object):
         if spot is None:
             return None
         else:
-            spot.park_vehicle(vehicle)
+            if vehicle.spot_size > 1:
+                self._park_starting_at_spot(spot, vehicle)
+            else:
+                spot.park_vehicle(vehicle)
             return spot
 
     def _find_available_spot(self, vehicle):
         """Find an available spot where vehicle can fit, or return None"""
-        pass
+        if vehicle.spot_size == 1:
+            for spot in self.spots:
+                if spot.can_fit_vehicle(vehicle):
+                    return spot
+            return None
+
+        consecutive = []
+        current_row = None
+        for spot in self.spots:
+            if current_row is None or spot.row != current_row:
+                consecutive = []
+                current_row = spot.row
+            if spot.can_fit_vehicle(vehicle):
+                consecutive.append(spot)
+                if len(consecutive) == vehicle.spot_size:
+                    return consecutive[0]
+            else:
+                consecutive = []
+        return None
 
     def _park_starting_at_spot(self, spot, vehicle):
         """Occupy starting at spot.spot_number to vehicle.spot_size."""
-        pass
+        start_index = self.spots.index(spot)
+        for index in range(start_index, start_index + vehicle.spot_size):
+            self.spots[index].park_vehicle(vehicle)
 
 
 class ParkingSpot(object):
@@ -119,7 +155,19 @@ class ParkingSpot(object):
         return vehicle.can_fit_in_spot(self)
 
     def park_vehicle(self, vehicle):
-        pass
+        if not self.can_fit_vehicle(vehicle):
+            return False
+        self.vehicle = vehicle
+        vehicle.take_spot(self)
+        self.level.available_spots -= 1
+        return True
 
     def remove_vehicle(self):
-        pass
+        if self.vehicle:
+            vehicle = self.vehicle
+            self.vehicle = None
+            self.level.spot_freed()
+            if self in vehicle.spots_taken:
+                vehicle.spots_taken.remove(self)
+            return vehicle
+        return None
